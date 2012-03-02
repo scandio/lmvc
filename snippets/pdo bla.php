96dfebc4fd24ce015fd0f6846ba3f996fb130f1b
Christian Koch <koch.chris@gmail.com>
2012-03-02 23:02:02 +0100

    delete some senseless files

--- snippets/pdo bla.php
+++
@@ -1,7 +0,0 @@
-<?php
-	function test() {
-		var_dump(App::get()->pdoConnection()->exec("create table test (name char(50))"));
-		var_dump(App::get()->pdoConnection()->exec("insert into test (name) values ('Max Mustermann')"));
-		var_dump(App::get()->pdoConnection()->query("select * from test")->fetchAll());
-	}
-		
