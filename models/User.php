c3a601f8431e25f682f5e43c0b71032f6f41aae4
Christian Koch <koch.chris@gmail.com>
2012-03-28 19:41:40 +0200

    new models

---
+++ models/User.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: ckoch
+ * Date: 28.03.12
+ * Time: 19:41
+ * To change this template use File | Settings | File Templates.
+ */
+class User
+{
+
+}
