dd24b35b0cad2993a2f2ec1cfc9031ce45c79c8b
Christian Koch <koch.chris@gmail.com>
2012-03-29 19:17:20 +0200

    new models

--- models/User.php
+++ models/User.php
@@ -1,12 +1,33 @@
 <?php
-/**
- * Created by JetBrains PhpStorm.
- * User: ckoch
- * Date: 28.03.12
- * Time: 19:41
- * To change this template use File | Settings | File Templates.
- */
-class User
-{
+
+$user = new User();
+$user->username = 'ckoch';
+
+define('ONE_TO_MANY_RELATION', 'ONE_TO_MANY_RELATION');
+
+class User extends Model {
+
+    private $id;
+    public $username;
+    public $passowrd;
+    public $fullname;
+
+    public $tweets = ONE_TO_MANY_RELATION;
+
+}
+
+class Model {
+
+    function save() {
+
+    }
+
+    function delete() {
+
+    }
+
+    static function find() {
+
+    }
 
 }
