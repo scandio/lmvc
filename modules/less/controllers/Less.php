18d319247974118c77d62b40ae040f52422c5a2b
Christian Koch <koch.chris@gmail.com>
2012-04-24 15:33:06 +0200

    on the fly less compile use uri/less/compile/<file-path> for rel

--- modules/less/controllers/Less.php
+++ modules/less/controllers/Less.php
@@ -3,8 +3,9 @@
 class Less extends Controller {
 
     public static function compile() {
+        $file = implode('/', App::get()->params);
         include('lessc.inc.php');
-        $lessCompiler = new lessc(App::get()->request->file);
+        $lessCompiler = new lessc(App::get()->config->appPath . $file);
         header('Content-Type: text/css');
         echo $lessCompiler->parse();
     }
