ecc2219895fe06480923bfb24718127dc2c9d282
Roland Schmid <r.s.priv@gmx.de>
2013-03-18 17:38:40 +0100

    Added Possibility to register Snippet-Directories

--- lib/Scandio/lmvc/modules/snippets/framework/SnippetHandler.php
+++ lib/Scandio/lmvc/modules/snippets/framework/SnippetHandler.php
@@ -8,6 +8,7 @@ abstract class SnippetHandler {
 
     protected static $snippetFile;
     protected static $prefix='';
+    protected static $snippetPath = array();
 
     public static function __callStatic($name, $params) {
         $result = "";
@@ -16,30 +17,52 @@ abstract class SnippetHandler {
         } else {
             $app = LVC::get();
 
-            self::$snippetFile = $app->config->appPath .
-                'snippets' . DIRECTORY_SEPARATOR . static::$prefix . LVC::camelCaseTo($name) . '.html';
-            if (!file_exists(self::$snippetFile)) {
-                $reflection = new ReflectionClass(get_called_class());
-                $classFileName = $reflection->getFileName();
-                $pathElements = explode(DIRECTORY_SEPARATOR, $classFileName);
-                if ($pathElements[count($pathElements)-4] == 'modules') {
-                    $module = $pathElements[count($pathElements)-3];
-                    $modulePath = DIRECTORY_SEPARATOR .
-                        implode(DIRECTORY_SEPARATOR, array_slice($pathElements, 1, count($pathElements)-4)) .
-                        DIRECTORY_SEPARATOR;
-                }
-                if ($module) {
-                    self::$snippetFile = $modulePath . $module . DIRECTORY_SEPARATOR .
-                        'snippets' . DIRECTORY_SEPARATOR . static::$prefix . LVC::camelCaseTo($name) . '.html';
-                }
-            }
-            if (file_exists(self::$snippetFile)) {
+            self::$snippetFile = self::searchSnippet(static::$prefix . LVC::camelCaseTo($name) . '.html');
+            if (self::$snippetFile) {
                 include(self::$snippetFile);
             } else {
                 $result = "\n<!-- No snippet file for " . get_called_class() . "::" . $name . "() exists. -->\n";
             }
         }
         return $result;
+    }
+
+    /**
+     * registers a new snippet directory to search for the snippets
+     *
+     * @param array|string $path specifies the directory to register
+     */
+    public static function registerSnippetDirectory($path) {
+        if (is_array($path)) {
+            $snippetPath = implode(DIRECTORY_SEPARATOR, $path);
+        } elseif (is_string($path)) {
+            $snippetPath = $path;
+        } else {
+            // TODO: Error - couldn't register SnippetDirectory
+            return;
+        }
+
+        $class = get_called_class();
+        if (isset(self::$snippetPath[$class])) {
+            array_unshift(self::$snippetPath[$class], $snippetPath);
+        } else {
+            self::$snippetPath[$class] = array($snippetPath);
+        }
+    }
+
+    /**
+     * searches for the snippet in the registered directories
+     *
+     * @return string|bool either the snippet's full path or false
+     */
+    private function searchSnippet($snippet) {
+        foreach (self::$snippetPath as $path) {
+            $snippetPath = LVC::get()->config->appPath . $path . DIRECTORY_SEPARATOR . $snippet;
+            if (file_exists($snippetPath)) {
+                return $snippetPath;
+            }
+        }
+        return false;
     }
 
 }
